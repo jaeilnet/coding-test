a84094e107995ef4979df286f9397a6a2ad02eb4
jaeil.joo <sgsgself@naver.com>
2022-08-29 22:25:12 +0900

    [unrated] Title: 성격 유형 검사하기, Time: 0.64 ms, Memory: 30.2 MB -BaekjoonHub

--- 프로그래머스/unrated/118666. 성격 유형 검사하기/성격 유형 검사하기.js
+++ 프로그래머스/unrated/118666. 성격 유형 검사하기/성격 유형 검사하기.js
@@ -1,32 +1,24 @@
 function solution(survey, choices) {
-  const types = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
-  let answer = "";
-  for (let i = 0; i < choices.length; i++) {
-    const [leftType, rightType] = survey[i].split("");
-      console.log(leftType, rightType)
-    const result = Math.abs(choices[i] - 4);
-    if (choices[i] < 4) {
-      // 비동의 -> 왼쪽 요소
-      types[leftType] += result;
-    } else if (choices[i] > 4) {
-      // 동의 -> 오른쪽 원소
-      types[rightType] += result;
-    }
-  }
+    var answer = '';
     
-  const type = Object.keys(types);
-  for (let i = 0; i < type.length; i += 2) {
-      
-    const leftType = types[type[i]];
-    const rightType = types[type[i + 1]];
-    if (leftType >= rightType) {
-      // 같거나 클 때는 왼쪽 요소
-      answer += type[i];
-    } else {
-      // 작은 때는 오른쪽 요소
-      answer += type[i + 1];
+    const obj = {}
+    
+    const type = [ "RT", "CF", 'JM', "AN"]
+    
+    for(let i of type){
+        const split = i.split("").forEach((e) => {
+            obj[e] = 0
+        })
     }
-  }
-
-  return answer;
+    
+    choices.forEach((e, i) => {
+        const [disagree, agree] = survey[i]
+    
+        obj[e > 4 ? agree : disagree] += Math.abs(e - 4)
+    
+    })
+    
+    const result = type.map(([a,b]) => obj[b] > obj[a] ? b : a).join("")
+    
+    return result;
 }
