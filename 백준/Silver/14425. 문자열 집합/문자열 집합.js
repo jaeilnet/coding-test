60f2f7bd2eefa40025799b529806f6d29ac53cbe
jaeil.joo <sgsgself@naver.com>
2022-10-17 13:11:18 +0900

    [Silver III] Title: 문자열 집합, Time: 228 ms, Memory: 32340 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.js
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.js
@@ -8,13 +8,22 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
 const [n] = input.shift().split(" ").map(Number);
 const strInput = input.map((i) => i.trim());
+// console.log(strInput);
 const list = strInput.slice(0, n);
 const candidate = strInput.slice(n);
 const set = new Set(list);
 
-let ans = 0;
-candidate.forEach((str) => {
-  if (set.has(str)) ++ans;
+// let ans = 0;
+// candidate.forEach((str) => {
+//   if (set.has(str)) ++ans;
+// });
+
+// console.log(ans);
+
+let count = 0;
+
+candidate.forEach((e) => {
+  if (set.has(e)) ++count;
 });
 
-console.log(ans);
+console.log(count);
