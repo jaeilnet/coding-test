841384b5f893821652c0b9bfc98566aff8e349cb
jaeil.joo <sgsgself@naver.com>
2022-10-17 13:14:33 +0900

    [Silver III] Title: 문자열 집합, Time: 220 ms, Memory: 32208 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.js
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.js
@@ -1,24 +1,25 @@
-
 const fs = require("fs");
+
 const filePath =
   process.platform === "linux"
     ? "/dev/stdin"
     : "./준/집합과 맵/14425/input.txt";
+
+// const [[n], ...S] = fs.readFileSync(filePath).toString().trim().split("\n");
 const input = fs.readFileSync(filePath).toString().trim().split("\n");
 
+// const M = S.splice(0, a[0]);
+
+// console.log(M, S);
+
+// const input = [...S].map((e) => e.trim());
+
 const [n] = input.shift().split(" ").map(Number);
-const strInput = input.map((i) => i.trim());
-// console.log(strInput);
-const list = strInput.slice(0, n);
-const candidate = strInput.slice(n);
+
+const list = input.slice(0, n);
+const candidate = input.slice(n);
+// console.log(list, candidate);
 const set = new Set(list);
-
-// let ans = 0;
-// candidate.forEach((str) => {
-//   if (set.has(str)) ++ans;
-// });
-
-// console.log(ans);
 
 let count = 0;
 
@@ -27,3 +28,24 @@ candidate.forEach((e) => {
 });
 
 console.log(count);
+
+// const fs = require("fs");
+// const filePath =
+//   process.platform === "linux"
+//     ? "/dev/stdin"
+//     : "./준/집합과 맵/14425/input.txt";
+// const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+// const [n] = input.shift().split(" ").map(Number);
+// const strInput = input.map((i) => i.trim());
+// console.log(strInput);
+// const list = strInput.slice(0, n);
+// const candidate = strInput.slice(n);
+// const set = new Set(list);
+
+// let ans = 0;
+// candidate.forEach((str) => {
+//   if (set.has(str)) ++ans;
+// });
+
+// console.log(ans);
