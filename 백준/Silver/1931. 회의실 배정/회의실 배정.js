e4c5ddc24f3a1147c10af3651407eaf6269730e4
jaeil.joo <sgsgself@naver.com>
2022-10-20 15:53:52 +0900

    [Silver I] Title: 회의실 배정, Time: 404 ms, Memory: 43396 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.js
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.js
@@ -1,15 +1,17 @@
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
-const N = Number(input[0]);
-input.shift();
-input = input.map((element) => element.split(" "));
-input.sort((a, b) => {
-  // 회의가 끝나는 시간을 기준으로 정렬, 끝나는 시간이 같다면 일찍 시작하는 순으로 정렬
-  a[0] = Number(a[0]);
-  a[1] = Number(a[1]);
-  b[0] = Number(b[0]);
-  b[1] = Number(b[1]);
+const fs = require("fs");
 
+const filePath =
+  process.platform === "linux" ? "/dev/stdin" : "./준/그리디/1931/input.txt";
+
+const [N, ...input] = require("fs")
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n");
+
+let arr = input.map((e) => e.split(" ").map(Number));
+
+arr.sort((a, b) => {
   if (a[1] === b[1]) {
     return a[0] - b[0];
   }
@@ -17,19 +19,21 @@ input.sort((a, b) => {
 });
 
 let count = 0;
-let previousEnd = 0;
-let currentStart;
-let currentEnd;
+let prevEnd = 0;
+let start = 0;
+let end = 0;
+
 for (let i = 0; i < N; i++) {
-  currentStart = input[i][0];
-  currentEnd = input[i][1];
-  if (currentStart < previousEnd) {
-    // 현재 회의 시작시간이 이전 회의 종료시간보다 일찍 시작하면 넘어가기
+  start = arr[i][0];
+  end = arr[i][1];
+
+  if (start < prevEnd) {
     continue;
   } else {
     count++;
   }
-  previousEnd = currentEnd;
+
+  prevEnd = end;
 }
 
 console.log(count);
