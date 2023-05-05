e8c874ccc53d73de6a573251a24f6a0c1eb6bcbc
jaeil.joo <sgsgself@naver.com>
2023-05-05 14:09:22 +0900

    [level 1] Title: 삼총사, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -0,0 +1,15 @@
+function solution(n) {
+    var answer = 0;
+    
+    for(let i=0; i<n.length; i++){
+        for(let j=i+1; j<n.length; j++){
+            for(let k=j+1; k<n.length; k++){
+                if(n[i] + n[j] + n[k] === 0){
+                    answer ++
+                }
+            }
+        }
+        
+    }
+    return answer;
+}
