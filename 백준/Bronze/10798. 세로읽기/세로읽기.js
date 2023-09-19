362ef28c34573fad386457a9059aa172d7155945
jaeil.joo <sgsgself@naver.com>
2023-09-19 13:33:02 +0900

    [Bronze I] Title: 세로읽기, Time: 136 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.js
+++ 백준/Bronze/10798. 세로읽기/세로읽기.js
@@ -1,36 +1,9 @@
-const fs = require("fs");
-
-const filePath =
-  process.platform === "linux" ? "/dev/stdin" : "./problem/input.txt";
-
-const input = fs
-  .readFileSync(filePath)
-  .toString()
-
-  .split("\n")
-  .map((el) => el.split(""));
-
-const loop = (input) => {
-  let len = Math.max(...input.map((i) => i.length));
-  let count = 0;
-
-  let str = [];
-
-  const loops = (count) => {
-    if (len === count) return str;
-
-    for (let i = 0; i < input.length; i++) {
-      str.push(input[i][count]);
+const words = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+const maxLength = Math.max(...words.map(i => i.length));
+let vertical = "";
+for (let i=0; i<maxLength; i++) {
+    for (let j=0; j<words.length; j++) {
+        vertical += words[j][i] || "";
     }
-    count++;
-    loops(count);
-  };
-
-  loops(count);
-
-  return str;
-};
-
-const result = loop(input);
-
-console.log(result.join(""));
+}
+console.log(vertical);
